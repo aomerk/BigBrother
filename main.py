bd9ff8ac408a9a0c3b537833a66dc0dbb8523bd3
aomerk <aliomerkeser@gmail.com>
2020-05-03 17:40:50 +0000

    extract function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+
 import cv2
 
 # add compilation flags
@@ -8,17 +9,25 @@ parser.add_argument("-t", "--type", help="Type of data to find faces from")
 
 args = parser.parse_args()
 
-# Handle images
-if args.type == "image":
-    test_image_directory = 'dataset/images/face/'
 
-    #  loop through all test images
-    for filename in os.listdir(test_image_directory):
+# Handle images
+def handle_images_in_directory(dirname):
+    for filename in os.listdir(dirname):
         if filename.endswith(".jpg") or filename.endswith(".jpeg") or filename.endswith(".png"):
-            frame = cv2.imread(test_image_directory + filename)
+            frame = cv2.imread(dirname + filename)
             cv2.imshow(filename, frame)
             cv2.waitKey(0)
             cv2.destroyWindow(filename)
+
+
+if args.type == "image":
+    test_face_image_directory = 'data/images/detection/face/'
+    #  loop through all face images
+    handle_images_in_directory(test_face_image_directory)
+
+    test_no_face_image_directory = 'data/images/detection/no-face/'
+    handle_images_in_directory(test_no_face_image_directory)
+
 
 #  Handle video input
 elif args.type == "video":
