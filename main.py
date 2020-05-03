e5879847f3bf79efce6a6b8c9058f733b46e7155
aomerk <aliomerkeser@gmail.com>
2020-05-03 17:31:33 +0000

    handle image-video-stream cases

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ parser.add_argument("-t", "--type", help="Type of data to find faces from")
 
 args = parser.parse_args()
 
+# Handle images
+if args.type == "image":
+    test_image_directory = 'dataset/images/face/'
+
     #  loop through all test images
     for filename in os.listdir(test_image_directory):
         if filename.endswith(".jpg") or filename.endswith(".jpeg") or filename.endswith(".png"):
@@ -15,3 +19,16 @@ args = parser.parse_args()
             cv2.imshow(filename, frame)
             cv2.waitKey(0)
             cv2.destroyWindow(filename)
+
+#  Handle video input
+elif args.type == "video":
+    print("not implemented yet.")
+    exit(1)
+
+# Handle stream input
+elif args.type == "stream":
+    print("not implemented yet")
+    exit(1)
+# Unknown input type
+else:
+    print("-t flag must be either video or image")
