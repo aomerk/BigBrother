dcc0e46a8729624ab806971391d2d9c6f2b8aa12
aomerk <aliomerkeser@gmail.com>
2020-05-03 17:31:19 +0000

    load and read images from datas

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-t", "--type", help="Type of data to find faces from")
 
 args = parser.parse_args()
+
+    #  loop through all test images
+    for filename in os.listdir(test_image_directory):
+        if filename.endswith(".jpg") or filename.endswith(".jpeg") or filename.endswith(".png"):
+            frame = cv2.imread(test_image_directory + filename)
+            cv2.imshow(filename, frame)
+            cv2.waitKey(0)
+            cv2.destroyWindow(filename)
